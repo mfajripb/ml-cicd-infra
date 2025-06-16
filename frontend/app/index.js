33d351297a427c1c61730cda37420bf897af6a12
mfajripb <91777728+mfajripb@users.noreply.github.com>
2025-06-16 07:35:14 +0700

    Update index.js

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 
 async function main() {
-  const response = await axios.post("http://<EC2_PUBLIC_IP>:5000/generate", {
+  const response = await axios.post("http://3.91.200.135:5000/generate", {
     prompt: "Once upon a time"
   });
   console.log("Generated:", response.data.response);
